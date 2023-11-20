71d96e5c570d31d0eea0658bc1eaabcc26173f7c
Katharina Ven <katharina.ven@helsinki.fi>
2023-11-20 10:30:05 +0200

    push

--- 'create_alc.R
+++ 'create_alc.R
@@ -1,8 +1,8 @@
 #Chapter 3: Logistic regression
 #Date: 13.11.2023
-#Katharina Ven    
- 
-#Data wrangling (max 5 points) 
+#Katharina Ven  
+
+#Data wrangling (max 5 points)
 
 #reading the csv files into script:
 math <- read.csv("student-mat.csv", sep = ';')
@@ -28,28 +28,38 @@ math_por <- inner_join(math, por, by = join_cols, suffix = c(".math", ".por"))
 #dimensions of table
 dim(math_por) #370 observations and  39 variables
 
-#getting rid of duplicate values with my own solution:
-#checking first if there are duplicate rows in datatable
-a <- duplicated(math_por)
-summary(a)
-#FALSE = there does not seem to be duplicate values?
-#trying anyway to remove duplicate rows with unique nad distinct commands
-aa <- unique(math_por)
-dim(aa) #370 39
-aaa <- distinct(math_por)
-dim(aaa) #370 39
-#after removing, still having same dimensions as in original table math_por
-#assuming that there are no duplicate rows
+#getting rid of duplicate values:
+# create a new data frame with only the joined columns
+alc <- select(math_por, all_of(join_cols))
+# print out the columns not used for joining (those that varied in the two data sets)
+# for every column name not used for joining...
+for(col_name in free_cols) {
+  # select two columns from 'math_por' with the same original name
+  two_cols <- select(math_por, starts_with(col_name))
+  # select the first column vector of those two columns
+  first_col <- select(two_cols, 1)[[1]]
+  
+  # then, enter the if-else structure!
+  # if that first column vector is numeric...
+  if(is.numeric(first_col)) {
+    # take a rounded average of each row of the two columns and
+    # add the resulting vector to the alc data frame
+    alc[col_name] <- round(rowMeans(two_cols))
+  } else { # else (if the first column vector was not numeric)...
+    # add the first column vector to the alc data frame
+    alc[col_name] <- first_col
+  }
+}
 
 library(dplyr); library(ggplot2)
 #new column 'alc_use' with average of Dalc (workday alcohol consumption) 
 #and Walc (weekend alcohol consumption)
-math_por2 <- mutate(math_por, alc_use = (Dalc + Walc) / 2)
+math_por2 <- mutate(alc, alc_use = (Dalc + Walc) / 2)
 #new column 'high_use' and if alc_use average above 2 (>2), high_use=TRUE 
 math_por3 <- math_por2 %>% mutate(income_na = if_else(alc_use > 2, TRUE, FALSE))
 
 library(readr)
 #glimpsing and saving joined and modified data
 glimpse(math_por3)
-dim(math_por3) #370 observations and  41 variables (+2 with two new columns we made)
+dim(math_por3) #370 observations and  35 variables
 write_csv(x=math_por3, 'math_por3.csv', col_names = TRUE)
